231930208170faf58117083bc7350f37c806adfb
hiromi.suda <hsuda@bns-g.com>
2021-10-25 10:22:02 +0000

    CRUD tasks

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,25 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'TasksController@index');
+
+Route::resource('tasks', 'TasksController');
+
+/*
+//CRUD
+// タスクの個別詳細ページ表示
+Route::get('tasks/{id}', 'TasksController@show');
+// タスクの新規登録を処理（新規登録画面を表示するものではない）
+Route::post('tasks', 'TasksController@store');
+// タスクの更新処理（編集画面を表示するものではない）
+Route::put('tasks/{id}', 'TasksController@update');
+// タスクを削除⇒これは不要かも
+Route::delete('tasks/{id}', 'TasksController@destroy');
+
+// index: showの補助ページ
+Route::get('tasks', 'TasksController@index')->name('tasks.index');
+// create: 新規作成用のフォームページ
+Route::get('tasks/create', 'TasksController@create')->name('tasks.create');
+// edit:　更新用のフォームページ
+Route::get('tasks/{id}/edit', 'TasksController@edit')->name('tasks.edit');
+*/
